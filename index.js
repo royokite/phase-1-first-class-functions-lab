14c95ba67173f2fc4d5abbd9907ed516389564d7
RoyOkite <royokite@gmail.com>
2022-08-17 15:20:29 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file!
+// First-class functions lab!
+
+const returnFirstTwoDrivers = arrayOFDrivers => arrayOFDrivers.slice(0, 2);
+
+const returnLastTwoDrivers = arrayOFDrivers => arrayOFDrivers.slice(-2);
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier) {
+    return (fareMultiplier = fare => fare * multiplier);
+}
+
+function fareDoubler(fare) {
+    const doubles = createFareMultiplier(2)(fare);
+    return doubles;
+}
+
+function fareTripler(fare) {
+    const triples = createFareMultiplier(3)(fare);
+    return triples;
+}
+
+function selectDifferentDrivers(arrayOFDrivers, returnFunction) {
+    return returnFunction===selectingDrivers[0]? returnFirstTwoDrivers(arrayOFDrivers): returnLastTwoDrivers(arrayOFDrivers);
+}
